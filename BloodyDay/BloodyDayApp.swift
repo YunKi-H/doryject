a1b3a8d8d79b458dd8ff0a4adb32b3481ac92f3f
Yunki-H <yunki.dev@gmail.com>
2025-10-09 21:05:56 +0900

    Initial setting

---
+++ BloodyDay/BloodyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodyDayApp.swift
+//  BloodyDay
+//
+//  Created by Yunki on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BloodyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BloodyDay/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  BloodyDay
+//
+//  Created by Yunki on 10/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
