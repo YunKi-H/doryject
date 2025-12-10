228a50b74c87e70448eca57b09fec90594f1823c
Yunki-H <yunki.dev@gmail.com>
2025-12-11 02:40:54 +0900

    feat: #17 알림 설정화면 UI

--- BloodyDay/Features/Settings/Notification/NotificationSettingView.swift
+++ BloodyDay/Features/Settings/Notification/NotificationSettingView.swift
@@ -8,9 +8,64 @@
 import SwiftUI
 
 struct NotificationSettingView: View {
+    @State private var periodDueDateNotify: Bool = false
+    @State private var periodDelayedNotify: Bool = false
+    
+    @State private var pillTakingNotify: Bool = false
+    @State private var pillBuyingNotify: Bool = false
+    
     var body: some View {
         VStack {
-            Text("hi")
+            List {
+                Section {
+                    Toggle(isOn: $periodDueDateNotify) {
+                        VStack(alignment: .leading) {
+                            Text("생리 예정일")
+                                .font(.regular_18)
+                                .foregroundStyle(.textPrimary)
+                            Text("시작 예정일 이틀 전 09:00")
+                                .font(.regular_14)
+                                .foregroundStyle(periodDueDateNotify ? .mainRed : .textTertiary)
+                        }
+                    }
+                    .tint(.mainRed)
+                    
+                    Toggle(isOn: $periodDelayedNotify) {
+                        Text("생리 지연")
+                            .font(.regular_18)
+                            .foregroundStyle(.textPrimary)
+                    }
+                    .tint(.mainRed)
+                }
+                
+                Section {
+                    Toggle(isOn: $pillTakingNotify) {
+                        VStack(alignment: .leading) {
+                            Text("피임약 복용")
+                                .font(.regular_18)
+                                .foregroundStyle(.textPrimary)
+                            Text("매일 12:40")
+                                .font(.regular_14)
+                                .foregroundStyle(pillTakingNotify ? .subBlue : .textTertiary)
+                        }
+                    }
+                    .tint(.subBlue)
+                    
+                    Toggle(isOn: $pillBuyingNotify) {
+                        VStack(alignment: .leading) {
+                            Text("피임약 구매")
+                                .font(.regular_18)
+                                .foregroundStyle(.textPrimary)
+                            Text("복용 예정일 하루 전 16:00")
+                                .font(.regular_14)
+                                .foregroundStyle(pillBuyingNotify ? .subBlue : .textTertiary)
+                        }
+                    }
+                    .tint(.subBlue)
+                }
+            }
+            .listSectionSpacing(14)
+            .contentMargins(.top, 14)
         }
         .toolbar {
             ToolbarItem(placement: .title) {
