81d3506464c6b08f7fbf38a38ba632cf3cadc56e
Yunki-H <yunki.dev@gmail.com>
2026-03-21 00:57:20 +0900

    feat: #102 위젯 타겟 추가

---
+++ BloodyDayWidget/BloodyDayWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  BloodyDayWidgetBundle.swift
+//  BloodyDayWidget
+//
+//  Created by Yunki on 3/21/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BloodyDayWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BloodyDayWidget()
+        BloodyDayWidgetControl()
+    }
+}
